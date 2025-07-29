88b8913b011e055ddcd78ed9b2f9db25b60a8388
talhazulfiqar17 <zulfiqartalha908@gmail.com>
2025-07-29 10:09:52 +0500

    Create public.js

---
+++ public/js/public.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    // Front-end interactions can be added here
+    // Currently no front-end JS functionality is required
+    
+    // Example: Track meta tag clicks (if implementing click tracking)
+    /*
+    $('meta[name="description"]').on('click', function() {
+        console.log('Description meta tag clicked');
+    });
+    */
+});
